edcb095135bd5b21965d64b322baec4eb6f85783
pgpetrov <petergpetrov@Peters-MacBook-Pro.local>
2015-11-02 15:36:00 +0200

    curry solutions
    solution

--- curry/curry.js
+++ curry/curry.js
@@ -1,24 +1,13 @@
 
-
-var gparams = [];
-var globalF;
-
-
 var curry = function(f) {
-  gparams = [];
-  globalF = f;
   var result = function() {
-    var index;
-    for (index = 0; index < arguments.length; ++index) {
-        gparams.push(arguments[index]);
-    }
-
-    if (gparams.length == globalF.length) {
-      var arg = gparams;
-      gparams = [];
-      return globalF.apply(null, arg);
+    var args = [].slice.call(arguments);
+    if (args.length >= f.length) {
+      return f.apply(null, args);
     } else {
-      return result;
+      return function() {
+        return result.apply(null, args.concat([].slice.call(arguments)));
+      }
     }
   };
   return result;
